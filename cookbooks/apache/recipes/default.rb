508cde2ce323fa9f97071166694358db6644d4e0
root <root@chef-server.tiger.cn>
2014-07-01 06:13:54 -0700

    apache

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package_name = 'apache2'
+service_name = 'apache2'
+document_root = '/var/www'
+
+if platform_family? 'rhel'
+  package_name = 'httpd'
+  service_name = 'httpd'
+  document_root = '/var/www/html'
+end
+
+package package_name do
+  action :install
+end
+
+service service_name do
+  action [ :enable, :start ]
+end
+
+template "#{document_root}/index.html" do
+  source "index.html.erb"
+  mode '0644'
+end
